133151df34e72e93e7f9b5ddf0f8702e066f2a6e
ukirdeomkar <ukirdeomkar4@gmail.com>
2023-06-27 20:49:25 +0530

    pagination added

--- src/components/News.js
+++ src/components/News.js
@@ -2,38 +2,79 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  articles =[];
   constructor() {
     super();
     console.log("This is News Component COnstructor");
     this.state = {
-      articles : this.articles,
-      loading : false 
-    }
+      articles: [],
+      loading: false,
+      page: 1,
+
+    };
   }
-  async componentDidMount(){
-    console.log("Componet MOunted")
-    let url = "https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&apiKey=938ce42feda24155b3ef57ceeba42290"
-    let data = await fetch(url)
-    let parsedData = await data.json()
-    console.log(parsedData)
+  async componentDidMount() {
+    console.log("Componet MOunted");
+    this.setState({pageSize : 10})
+    let url =
+      `https://newsapi.org/v2/top-headlines?country=us&apiKey=938ce42feda24155b3ef57ceeba42290&page=1&pageSize=9`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({ articles: parsedData.articles  , totalResults : parsedData.totalResults });
+
+
+  }
+  handleNextClick = async () => {
+    console.log("next")
+
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=938ce42feda24155b3ef57ceeba42290&page=${this.state.page + 1}&pageSize=9`
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
     this.setState({articles : parsedData.articles})
-  }
+    this.setState({page  :this.state.page + 1})
+    
+  };
+  handlePrevClick = async () => {
+    console.log("prev");
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=938ce42feda24155b3ef57ceeba42290&page=${this.state.page - 1}&pageSize=9`
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({articles : parsedData.articles})
+    this.setState({page  :this.state.page - 1})
+  };
   render() {
-    console.log("render")
+    console.log("render");
     return (
       <>
         <div className="container my-3 ">
           <h2>Top Headlines</h2>
           <div className="row">
-            {this.state.articles.map((element)=>{
-              return <div className="col-md-4" key={element.url}>
-                <NewsItem title={element.title?element.title.slice(0,40):""} description={element.description?element.description.slice(0,66):""} imgUrl={element.urlToImage} newsUrl={element.url} />
-              </div>
-              
+            {this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 40) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 66)
+                        : ""
+                    }
+                    imgUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
             })}
-
-
+          </div>
+          <div className="container d-flex justify-content-between">
+            <button disabled={this.state.page<=1} className="btn btn-primary" onClick={this.handlePrevClick}>
+              &larr;Previous
+            </button>
+            <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/10)} className="btn btn-primary" onClick={this.handleNextClick}>
+              Next &rarr;
+            </button>
           </div>
         </div>
       </>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,14 +4,17 @@ export class NewsItem extends Component {
   render() {
     let {imgUrl , title, description  , newsUrl} = this.props;
     return (
-        <div className="card my-3" style={{width: "18rem"}}>
+      <div className="container">
+      <div className="card my-3" style={{width: "18rem"}}>
         <img src={!imgUrl?"https://techcrunch.com/wp-content/uploads/2023/04/GettyImages-1341075718.jpg?resize=1200,750":imgUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description}</p>
-          <a href={newsUrl} className="btn btn-sm btn-primary">Read More</a>
+          <a href={newsUrl} rel='noreferrer' target='_blank' className="btn btn-sm btn-primary">Read More</a>
         </div>
+      </div>       
       </div>
+
     )
   }
 }
